4de2c08648606bf038df0543bd072f90bc3bee9e
PushkarKolekar <pushkarkolekar09@gmail.com>
2023-02-21 19:29:33 +0530

    server.js api

--- server.js
+++ server.js
@@ -19,23 +19,26 @@ mongoose
   .catch(console.error);
 const Todo = require("./models/Todo");
 
-app.get("/todos", async (req, res) => {
-  const todo = await Todo.find();
-  res.json(todo);
+app.get('/todos', async (req, res) => {
+	const todos = await Todo.find();
+
+	res.json(todos);
 });
 
-app.post("/todo/new", (req, res) => {
-  const todo = new Todo({
-    text: req.body.text,
-  });
-  todo.save();
+app.post('/todo/new', (req, res) => {
+	const todo = new Todo({
+		text: req.body.text
+	})
 
-  res.json(todo);
+	todo.save();
+
+	res.json(todo);
 });
 
-app.delete("/todos/delete/:id", async (req, res) => {
-  const result = await Todo.findByIdAndDelete(req.params.id);
-  res.json(result);
+app.delete('/todo/delete/:id', async (req, res) => {
+	const result = await Todo.findByIdAndDelete(req.params.id);
+
+	res.json({result});
 });
 
 app.get('/todo/complete/:id', async (req, res) => {
@@ -47,12 +50,15 @@ app.get('/todo/complete/:id', async (req, res) => {
 
 	res.json(todo);
 })
-app.put("/todo/update/:id", async (req, res) => {
-  const todo = await Todo.findById(req.params.id);
-  todo.complete = !todo.complete;
 
-  todo.save();
-  res.json(todo);
+app.put('/todo/update/:id', async (req, res) => {
+	const todo = await Todo.findById(req.params.id);
+
+	todo.text = req.body.text;
+
+	todo.save();
+
+	res.json(todo);
 });
 
 app.listen(process.env.PORT || 5500, () => console.log("server started on port 5500"));
